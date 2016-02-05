0b2dafd9b1e5c281b27e2a061e7a8cb3784a6951
James Bremner <james@ravenspoint.com>
2016-02-04 22:41:08 -0500

    Zoom using mousewheel

--- main.cpp
+++ main.cpp
@@ -23,206 +23,259 @@ void SetupRC();
 void SpinDisplay();
 void mouse(int, int, int, int);
 
-static GLfloat spin = 0.0;
 static char nameoffile[256];
+
+class cCamera
+{
+public:
+    cCamera()
+        : myZoom( 1 )
+        , mySpin( 0 )
+    {
+
+    }
+
+    void Zoom( int dir )
+    {
+        if( dir > 0 )
+            myZoom += 0.1;
+        else
+            myZoom -= 0.1;
+        if( myZoom < 0.1 )
+            myZoom = 0.1;
+        cout << "Zoom " << myZoom;
+        glutPostRedisplay();
+    }
+
+    void WindowSize( int w, int h )
+    {
+        glViewport (0, 0, w, h);
+        myRatio = w/h;
+        glutPostRedisplay();
+    }
+
+    void Position()
+    {
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        gluPerspective (50.0*myZoom, myRatio, 10, 2000);
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+        gluLookAt( 0, -1000, 0,
+                   0, 0, 0,
+                   0, 0, 1 );
+    }
+
+    void setSpin()
+    {
+        // increment the spin a little on each mouse button click
+        mySpin += 20.0;
+        if( mySpin > 360 )
+            mySpin -= 360;
+        glutPostRedisplay();
+    }
+
+    float Spin()
+    {
+        return mySpin;
+    }
+
+private:
+
+    float myZoom;       // Ratio of zoom in/out to default;
+    float myRatio;           // window aspect ratio;
+    float mySpin;
+
+} theCamera;
+
+// called when mousewheel is turned
+void mousewheel(int button, int dir, int x, int y)
+{
+    theCamera.Zoom( dir );
+}
+// called when the window changes size
+void ChangeSize(GLsizei w, GLsizei h)
+{
+    theCamera.WindowSize( w, h );
+}
+
+// called when mouse button pressed
+void mouse(int button, int state, int x, int y)
+{
+    switch (button)
+    {
+    case GLUT_LEFT_BUTTON:
+        if (state == GLUT_DOWN)
+            theCamera.setSpin();
+        else
+            break;
+    case GLUT_MIDDLE_BUTTON:
+        if (state == GLUT_DOWN)
+            glutIdleFunc(NULL);
+        else
+            break;
+    default:
+        break;
+    }
+}
 
 int main(int argc, char* argv[])
 {
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-	char stringone[256] = "STL Viewer ";
-	strcat(stringone,nameoffile);
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    char stringone[256] = "STL Viewer ";
+    strcat(stringone,nameoffile);
 
-	glutCreateWindow(stringone);
+    glutCreateWindow(stringone);
 
-	SetupRC();
+    SetupRC();
 
-	glutDisplayFunc(RenderScene);
-	glutReshapeFunc(ChangeSize);
-	glutMouseFunc(mouse);
+    glutDisplayFunc(RenderScene);
+    glutReshapeFunc(ChangeSize);
+    glutMouseFunc(mouse);
+    glutMouseWheelFunc( mousewheel );
 
-	glutMainLoop();
+    glutMainLoop();
 
-	system ("pause");
-
-
-
+    system ("pause");
 
 
-	return 0;
+
+
+
+    return 0;
 }
 
 void SetupRC()
 {
-	glClearColor(0.0, 0.0, 0.0, 1.0);
-	glColor3f(0.0, 1.0, 0.0);
-	glShadeModel(GL_FLAT);
-	GLfloat ambientLight[]={0.3, 0.3, 0.3, 1.0};
-	GLfloat diffuseLight[]={0.7, 0.7, 0.7, 1.0};
-	GLfloat specular[]={1.0, 1.0, 1.0, 1.0};
-	GLfloat specref[]={1.0, 1.0, 1.0, 1.0};
-	GLfloat lightPos[]={0.0, 0.0, 75.0, 1.0};
-	glEnable(GL_LIGHTING);
-	glLightfv(GL_LIGHT0,GL_AMBIENT,ambientLight);
-	glLightfv(GL_LIGHT0,GL_DIFFUSE,diffuseLight);
-	glLightfv(GL_LIGHT0,GL_SPECULAR,specular);
-	glLightfv(GL_LIGHT0,GL_POSITION,lightPos);
-	glEnable(GL_LIGHT0);
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+    glColor3f(0.0, 1.0, 0.0);
+    glShadeModel(GL_FLAT);
+    GLfloat ambientLight[]= {0.3, 0.3, 0.3, 1.0};
+    GLfloat diffuseLight[]= {0.7, 0.7, 0.7, 1.0};
+    GLfloat specular[]= {1.0, 1.0, 1.0, 1.0};
+    GLfloat specref[]= {1.0, 1.0, 1.0, 1.0};
+    GLfloat lightPos[]= {0.0, 0.0, 75.0, 1.0};
+    glEnable(GL_LIGHTING);
+    glLightfv(GL_LIGHT0,GL_AMBIENT,ambientLight);
+    glLightfv(GL_LIGHT0,GL_DIFFUSE,diffuseLight);
+    glLightfv(GL_LIGHT0,GL_SPECULAR,specular);
+    glLightfv(GL_LIGHT0,GL_POSITION,lightPos);
+    glEnable(GL_LIGHT0);
 
-	glEnable(GL_COLOR_MATERIAL);
-	glColorMaterial(GL_FRONT,GL_AMBIENT_AND_DIFFUSE);
-	glMaterialfv(GL_FRONT,GL_SPECULAR,specref);
-	glMateriali(GL_FRONT,GL_SHININESS,128);
+    glEnable(GL_COLOR_MATERIAL);
+    glColorMaterial(GL_FRONT,GL_AMBIENT_AND_DIFFUSE);
+    glMaterialfv(GL_FRONT,GL_SPECULAR,specref);
+    glMateriali(GL_FRONT,GL_SHININESS,128);
 
-}
-
-void ChangeSize(GLsizei w, GLsizei h)
-{
-	float maxDistance = 100.0;
-	GLfloat maxRange = maxDistance;
-	if (h==0)
-		h=1;
-
-	glViewport (0, 0, w, h);
-
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	float ratio = w/h;
-	if (w<=h)
-		glOrtho (-maxRange, maxRange, -maxRange/ratio, maxRange/ratio, -maxRange,
-		maxRange);
-	else
-		glOrtho (-maxRange*ratio, maxRange*ratio, -maxRange, maxRange, -maxRange,
-		maxRange);
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
 }
 
 void RenderScene()
 {
-	string givenline;
-	vector<string> givenlines;
-	string title, eachline, facet, normal, vertex;
-	char linefromstandard[256];
-	float coordinatex, coordinatey, coordinatez;
-	int totalfacet=0, totalvertex=0;
-	int plutonium;
-	FILE* givenfile;
-	givenfile = fopen("coin_cargo.stl","r"); // The file you want to see. Currently it should be at the same
-	// folder with main.cpp. The file location depends on system spec.
-	/*givenfile = fopen ("anulus.stl","r");*/
-	/*givenfile = fopen ("SmallGear.stl","r");*/
-	/*givenfile = fopen ("handle.stl","r");*/
-	/*givenfile = fopen ("crank.stl","r");*/
+    // Move camera to required position
+    theCamera.Position();
 
-	vector <float> normalvector;
-	vector <float> trianglecoordinate;
-	if (givenfile != NULL)
-	{
-		/*cout << "There is the file" << endl;*/
-		fscanf(givenfile, "%s %s", linefromstandard, nameoffile);
-		/*cout << "The file name is " << nameoffile << endl;*/
+    string givenline;
+    vector<string> givenlines;
+    string title, eachline, facet, normal, vertex;
+    char linefromstandard[256];
+    float coordinatex, coordinatey, coordinatez;
+    int totalfacet=0, totalvertex=0;
+    int plutonium;
+    FILE* givenfile;
+    givenfile = fopen("coin_cargo.stl","r"); // The file you want to see. Currently it should be at the same
+    // folder with main.cpp. The file location depends on system spec.
+    /*givenfile = fopen ("anulus.stl","r");*/
+    /*givenfile = fopen ("SmallGear.stl","r");*/
+    /*givenfile = fopen ("handle.stl","r");*/
+    /*givenfile = fopen ("crank.stl","r");*/
 
-		int m=1;
-		while((char) fgetc(givenfile)!='e')
-		{
+    vector <float> normalvector;
+    vector <float> trianglecoordinate;
+    if (givenfile != NULL)
+    {
+        /*cout << "There is the file" << endl;*/
+        fscanf(givenfile, "%s %s", linefromstandard, nameoffile);
+        /*cout << "The file name is " << nameoffile << endl;*/
 
-			fscanf(givenfile, "%s %s %f %f %f", linefromstandard, linefromstandard,
-				&coordinatex, &coordinatey, &coordinatez);
-			totalfacet++;
-			/*cout << coordinatex << " " << coordinatey << " " << coordinatez << endl;*/
-			normalvector.push_back(coordinatex);
-			normalvector.push_back(coordinatey);
-			normalvector.push_back(coordinatez);
-			fscanf(givenfile, "%s %s", linefromstandard, linefromstandard);
-			for (int n=0; n<3; n++)
-			{
-				fscanf(givenfile, "%s %f %f %f", linefromstandard,
-					&coordinatex, &coordinatey, &coordinatez);
-				totalvertex++;
-				/*cout << coordinatex << " " << coordinatey << " " << coordinatez << endl;*/
-				trianglecoordinate.push_back(coordinatex);
-				trianglecoordinate.push_back(coordinatey);
-				trianglecoordinate.push_back(coordinatez);
-			}
-			fscanf(givenfile, "%s", linefromstandard);
-			fscanf(givenfile, "%s", linefromstandard);
-			if ((char) fgetc(givenfile)=='e')
-			{
-				break;
-				m=0;
-			}
-		}
-		fclose(givenfile);
-	}
-	else
-		cout << "I couldn't find the file or file is empty." << endl;
+        int m=1;
+        while((char) fgetc(givenfile)!='e')
+        {
 
-	string filename;
-	int totalfacetnumber=0;
-	int filenameline = 2;
-	int facetline = 2+2+3;
+            fscanf(givenfile, "%s %s %f %f %f", linefromstandard, linefromstandard,
+                   &coordinatex, &coordinatey, &coordinatez);
+            totalfacet++;
+            /*cout << coordinatex << " " << coordinatey << " " << coordinatez << endl;*/
+            normalvector.push_back(coordinatex);
+            normalvector.push_back(coordinatey);
+            normalvector.push_back(coordinatez);
+            fscanf(givenfile, "%s %s", linefromstandard, linefromstandard);
+            for (int n=0; n<3; n++)
+            {
+                fscanf(givenfile, "%s %f %f %f", linefromstandard,
+                       &coordinatex, &coordinatey, &coordinatez);
+                totalvertex++;
+                /*cout << coordinatex << " " << coordinatey << " " << coordinatez << endl;*/
+                trianglecoordinate.push_back(coordinatex);
+                trianglecoordinate.push_back(coordinatey);
+                trianglecoordinate.push_back(coordinatez);
+            }
+            fscanf(givenfile, "%s", linefromstandard);
+            fscanf(givenfile, "%s", linefromstandard);
+            if ((char) fgetc(givenfile)=='e')
+            {
+                break;
+                m=0;
+            }
+        }
+        fclose(givenfile);
+    }
+    else
+        cout << "I couldn't find the file or file is empty." << endl;
 
-	/*cout << "\n";
-	cout << totalfacet << " " << totalvertex/3 << " " << totalfacetnumber << endl;*/
-	cout << normalvector.size()/3 << " " << trianglecoordinate.size()/(3*3) << endl;
+    string filename;
+    int totalfacetnumber=0;
+    int filenameline = 2;
+    int facetline = 2+2+3;
 
-	int protactinium, neptunium;
+    /*cout << "\n";
+    cout << totalfacet << " " << totalvertex/3 << " " << totalfacetnumber << endl;*/
+    cout << normalvector.size()/3 << " " << trianglecoordinate.size()/(3*3) << endl;
 
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glEnable(GL_CULL_FACE);
-	glEnable(GL_DEPTH_TEST);
-	glPushMatrix();
-	float xRot=45.0, yRot=45.0, zRot=45.0;
-	glRotatef(xRot, 1.0, 0.0, 0.0);
-	glRotatef(spin, 0.0, 1.0, 0.0); // Somehow this spin valiable doesn't work.
-	glRotatef(zRot, 0.0, 0.0, 1.0);
+    int protactinium, neptunium;
 
-	int groupsize=9;
-	glBegin(GL_TRIANGLES);
-	for (protactinium=0; protactinium<totalfacet; protactinium++)
-	{
-		neptunium = groupsize*protactinium;
-		glVertex3f(trianglecoordinate.at(neptunium),trianglecoordinate.at(neptunium+1),
-			trianglecoordinate.at(neptunium+2));
-		glVertex3f(trianglecoordinate.at(neptunium+3),trianglecoordinate.at(neptunium+4),
-			trianglecoordinate.at(neptunium+5));
-		glVertex3f(trianglecoordinate.at(neptunium+6),trianglecoordinate.at(neptunium+7),
-			trianglecoordinate.at(neptunium+8));
-	}
-	glEnd();
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glEnable(GL_CULL_FACE);
+    glEnable(GL_DEPTH_TEST);
+    glPushMatrix();
+    float xRot=45.0, yRot=45.0, zRot=45.0;
+    glRotatef(xRot, 1.0, 0.0, 0.0);
+    glRotatef(theCamera.Spin(), 0.0, 1.0, 0.0);
+    glRotatef(zRot, 0.0, 0.0, 1.0);
 
-	glPopMatrix();
-	glutSwapBuffers();
+    int groupsize=9;
+    glBegin(GL_TRIANGLES);
+    for (protactinium=0; protactinium<totalfacet; protactinium++)
+    {
+        neptunium = groupsize*protactinium;
+        glVertex3f(trianglecoordinate.at(neptunium),trianglecoordinate.at(neptunium+1),
+                   trianglecoordinate.at(neptunium+2));
+        glVertex3f(trianglecoordinate.at(neptunium+3),trianglecoordinate.at(neptunium+4),
+                   trianglecoordinate.at(neptunium+5));
+        glVertex3f(trianglecoordinate.at(neptunium+6),trianglecoordinate.at(neptunium+7),
+                   trianglecoordinate.at(neptunium+8));
+    }
+    glEnd();
+
+    glPopMatrix();
+
+
+    glutSwapBuffers();
 }
 
-void SpinDisplay()
-{
-	// increment the spin a little on each mouse button click
-	spin += 20.0;
-	if( spin > 360 )
-        spin -= 360;
-	glutPostRedisplay();
-}
 
-void mouse(int button, int state, int x, int y)
-{
-	switch (button)
-	{
-		case GLUT_LEFT_BUTTON:
-			if (state == GLUT_DOWN)
-				SpinDisplay();
-			else
-				break;
-		case GLUT_MIDDLE_BUTTON:
-			if (state == GLUT_DOWN)
-				glutIdleFunc(NULL);
-			else
-				break;
-		default:
-			break;
-	}
-}
+
+
 
 
 
